f241364825bc90801ae1eab690a47a3a721e6dbd
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2024-07-11 19:48:15 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Digite o número de voltas:");
-		int numerovoltas=scanner.nextInt();
-		int[]temposvoltas=new int[numerovoltas];
-		int melhortempo=Integrer.MAX_VALUE;
-		int melhorvolta=-1;
-	    int somatempo=0;
-	    for(int i=0;i<numerovoltas;i++){
-		System.out.println("Digite o tempo de sua volta:"+(i+1)+":");
-		tempovoltas[i]=scanner.nextInt();
-		somatempo += tempovoltas[i];
-		if(tempovoltas[i] < melhortempo){
-		melhortempo=temposvoltas[i];
-		melhortempo=i + 1;
+	    Scanner input=new Scanner(System.in);
+	    Random random=new Random();
+	    int gerador=random.nextInt(1+100);
+	    int tentativa=0;
+		System.out.println("Tente adivinhar um número entre 1 á 100");
+		do{
+		    System.out.println("Informe um número");
+		    int num=input.nextInt();
+		    tentativa ++;
+		    if(num<gerador){
+		    System.out.println("Informe um número maior:");    
+		    }else if(num>gerador){
+		        System.out.println("Informe um número menor:");
+		    }else{
+		        System.out.println(String.format("Você conseguiu em %d tentativas",tentativa));
+		        break;
+		    }
+		    
+		     }while(true);
+		   
+	   		input.close();
+
 		}
-	    }
-		double tempomedio(double) somatempo/numerovoltas;
-		   System.out.println("O seu melhor de volta foi:"+ melhorvolta + "com o tempo de" + melhortempo + "segundos");
-		   System.out.println("O sua média de tempo foi:"+ tempomedio);
-		   scanner.close();
-		 
-		   }
-		
-//Criei um array temposVoltas para armazenar os tempos de cada volta.
-//Inicializei melhorTempo com Integer.MAX_VALUE e melhorVolta com -1 para encontrar o menor tempo e a volta correspondente.
-//Iterei sobre as voltas, solicitando o tempo de cada uma e atualizando a soma dos tempos.
-//Atualizei melhorTempo e melhorVolta sempre que um tempo menor era encontrado.
-//Calculei tempoMedio como a soma dos tempos dividida pelo número de voltas.
-//Adicionei a exibição da volta em que ocorreu o melhor tempo.
-}
+      }
